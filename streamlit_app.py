c103e404b45d190359da631b96258e0dc7ac3536
TamuNongera02 <103683485+TamuNongera02@users.noreply.github.com>
2022-08-10 20:07:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,36 +3,53 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import cv2
 
-"""
-# Welcome to Streamlit!
+# List of emotions 
+emotion =  ['Anger', 'Happy', 'Neutral', 'Surprise', 'Sad']
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# loads model
+model = keras.models.load_model("my_model.h5")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Select font 
+# font = cv2.FONT_HERSHEY_SIMPLEX
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Call the camera device=0.
+cam = cv2.VideoCapture(0)
 
+# Loads Harr Cascade file used to detect faces. 
+face_cas = cv2.CascadeClassifier('./cascades/haarcascade_frontalface_default.xml')
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# While Loop for inference 'while'  camera is on.
+while True:
+    ret, frame = cam.read()
+    
+    if ret==True:
+        # turn color into B/W images.
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # recongnise face.
+        faces = face_cas.detectMultiScale(gray, 1.3,5)
+        
+        for (x, y, w, h) in faces:
+            face_component = gray[y:y+h, x:x+w]
+            fc = cv2.resize(face_component, (48, 48))
+            inp = np.reshape(fc,(1,48,48,1)).astype(np.float32)
+            inp = inp/255.
+            prediction = model.predict(inp)
+            em = emotion[np.argmax(prediction)]
+            score = np.max(prediction)
+            
+            # create square and percentage.
+            cv2.putText(frame, em+"  "+str(score*100)+'%', (x, y), font, 1, (0, 255, 0), 2)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        cv2.imshow("image", frame)
+        
+        if cv2.waitKey(1) == 27:
+            break
+    else:
+        print ('Error')
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Close Camera.
+cam.release()
+cv2.destroyAllWindows()
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
